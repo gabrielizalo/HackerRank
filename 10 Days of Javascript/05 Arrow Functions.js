28c3f1b9ae10d91dcd590ec335ef3e31b126a1db
Gabriel Porras <ghporras@gmail.com>
2018-08-30 10:40:01 -0500

    Added 1 Exercise 5 of 10 Days of Javascript

---
+++ 10 Days of Javascript/05 Arrow Functions.js
@@ -0,0 +1,16 @@
+﻿/**
+ * Created by gporras on 2018-08-30.
+ */
+
+/*
+ * Modify and return the array so that all even elements are doubled and all odd elements are tripled.
+ *
+ * Parameter(s):
+ * nums: An array of numbers.
+ */
+function modifyArray ( nums ) {
+  const multiplyArray = nums.map ( function ( a ) {
+    return a % 2 === 0 ? a * 2 : a * 3;
+  } );
+  return multiplyArray;
+}
